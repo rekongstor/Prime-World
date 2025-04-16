6d9aad655ef8fb8cb4c3f7a7738f94f0d43dec68
Rekongstor <rekongstor@mail.ru>
2025-04-16 20:42:18 +0300

    Make links

---
+++ pw_publish/branch/Client/PvP/make-links.bat
@@ -0,0 +1,4 @@
+mklink /j /D Data C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Data\
+mklink /j /D Localization C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Localization\
+mklink /j /D Profiles  C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Profiles\
+mklink /j /D Tools  C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Tools\
